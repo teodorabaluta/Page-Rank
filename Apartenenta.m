a1429a79ea54ff817aec4d842713ddd1f16c8e41
Valentina Băluță <95169991+teodorabaluta@users.noreply.github.com>
2022-11-10 09:46:55 +0200

    Update Apartenenta.m

--- Apartenenta.m
+++ Apartenenta.m
@@ -1,17 +1,24 @@
 function y = Apartenenta(x, val1, val2)
 	% Functia care primeste ca parametrii x, val1, val2 si care calculeaza valoarea functiei membru in punctul x.
 	% Stim ca 0 <= x <= 1
-    dif = (val2 - val1); %am facut diferenta dintre cele 2 valori citite
-    a = 1 / dif;
-    b = -val1 / dif; %am calculat a si b astfel incat functia sa fie continua
-    %rezolvarea sistemului
-    if x < val1
-        y = 0;
-    end
+	
+	%se calculeaza diferenta celor doua valori citite
+	dif = (val2 - val1); 
+    	
+	%se calculeaza a si b astfel incat functia sa fie continua
+    	a = 1 / dif;
+    	b = -val1 / dif;
+    	
+	%rezolvarea sistemului
+    	if x < val1
+        	y = 0;
+    	end
 
-    if x >= val1 && x <= val2
-        y = a * x + b;
-    end
+    	if x >= val1 && x <= val2
+        	y = a * x + b;
+    	end
     
-    if x >= val2
-        y = 1;
+    	if x >= val2
+        	y = 1;
+	end
+end
